0ef2cace42de82118e123f1409aca0a6f344635e
Pedro Araújo <araujo.peo@gmail.com>
2022-03-15 23:57:01 -0300

    Add Comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_request, only: [:create]
   before_action :set_user, only: [:show, :destroy]
 
+  # GET /users
   def index
     @users = User.all
     render json: @users, status: :ok
